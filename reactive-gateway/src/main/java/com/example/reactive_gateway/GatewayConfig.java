605b45ac9b4004182863a9c04f5bc51bc4bc976c
SamAparicioDev <samapariciodev@gmail.com>
2024-09-03 18:13:26 -0500

    Configuracion del eureka server y del api reactive gateway

---
+++ reactive-gateway/src/main/java/com/example/reactive_gateway/GatewayConfig.java
@@ -0,0 +1,21 @@
+package com.example.reactive_gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user.webflux", route ->
+                        route.path("/api/v2/user/**")
+                                .uri("lb://user.webflux"))
+                .route("task.serve", route ->
+                route.path("/api/v2/task/**")
+                        .uri("lb://task.webflux"))
+                .build();
+    }
+}
